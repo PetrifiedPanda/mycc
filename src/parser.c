bcfd0245cca43ceb04f5a1ee6140abe4554431c5
PetrifiedPanda <bfj.koehn@gmail.com>
2021-10-25 09:37:48 +0200

    Forgot to change parser in last commit

--- src/parser.c
+++ src/parser.c
@@ -10,7 +10,7 @@ typedef struct {
 } ParserState;
 
 void expected_token_error(TokenType expected, const Token* got) {
-    set_error(ERR_PARSER, got->file, got->source_loc, "Expected token of type %s but got token of type %s", get_type_str(expected), get_type_str(got->type));
+    set_error_file(ERR_PARSER, got->file, got->source_loc, "Expected token of type %s but got token of type %s", get_type_str(expected), get_type_str(got->type));
 }
 
 bool accept(ParserState* s, TokenType expected, const char* filename) {
