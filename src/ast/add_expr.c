0986f486e866426e4d3bc8541391e0a511412288
PetrifiedPanda <bfj.koehn@gmail.com>
2021-11-03 11:51:21 +0100

    Added asserts for operators

--- src/ast/add_expr.c
+++ src/ast/add_expr.c
@@ -4,7 +4,11 @@
 #include <assert.h>
 
 AddExpr* create_add_expr(MulExpr* lhs, size_t len, MulExprAndOp* add_chain) {
-    assert(false); // TODO: assert ops
+    for (size_t i = 0; i < len; ++i) { 
+        assert(add_chain[i].rhs);
+        TokenType op = add_chain[i].add_op;
+        assert(op == ADD || op == SUB);
+    }
     assert(lhs);
     if (len != 0) {
         assert(add_chain);
