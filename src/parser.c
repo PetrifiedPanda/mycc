a6170c0addedebb986e4bbdbe9b43cc5b23dda06
PetrifiedPanda <bfj.koehn@gmail.com>
2021-11-03 13:55:49 +0100

    Made all functions in parser.c static

--- src/parser.c
+++ src/parser.c
@@ -9,11 +9,11 @@ typedef struct {
     const Token* it;
 } ParserState;
 
-void expected_token_error(TokenType expected, const Token* got) {
+static void expected_token_error(TokenType expected, const Token* got) {
     set_error_file(ERR_PARSER, got->file, got->source_loc, "Expected token of type %s but got token of type %s", get_type_str(expected), get_type_str(got->type));
 }
 
-bool accept(ParserState* s, TokenType expected) {
+static bool accept(ParserState* s, TokenType expected) {
     if (s->it->type != expected) {
         expected_token_error(expected, s->it);
         return false;
