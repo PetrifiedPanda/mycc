333d52c7e60d58d832a17bfe0910214a83f26226
Koehn <bfj.koehn@gmail.com>
2021-12-16 14:59:02 +0100

    Printed to stderr instead of stdout

--- test/test_asserts.h
+++ test/test_asserts.h
@@ -1,22 +1,23 @@
 #ifndef TEST_ASSERTS_H
 #define TEST_ASSERTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "token_type.h"
 #include "error.h"
 
 #define PRINT_ASSERT_ERR() \
-    printf("Assertion failure in %s, %d\n\t", __FILE__, __LINE__)
+    fprintf(stderr, "Assertion failure in %s, %d\n\t", __FILE__, __LINE__)
 
 
-#define assert_size_t(got, expected)                        \
-do {                                                        \
-    if ((got) != (expected)) {                              \
-        PRINT_ASSERT_ERR();                                 \
-        printf("Expected %zu but got %zu", expected, got);  \
-        exit(EXIT_FAILURE);                                 \
-    }                                                       \
+#define assert_size_t(got, expected)                                    \
+do {                                                                    \
+    if ((got) != (expected)) {                                          \
+        PRINT_ASSERT_ERR();                                             \
+        fprintf(stderr, "Expected %zu but got %zu", expected, got);     \
+        exit(EXIT_FAILURE);                                             \
+    }                                                                   \
 } while (0)
 
 
@@ -24,45 +25,45 @@ do {                                                        \
 do {                                                                                        \
     if (((expected) == NULL && (got) != NULL) || ((got) == NULL && (expected) != NULL)) {   \
         PRINT_ASSERT_ERR();                                                                 \
-        printf("Expected %s but got %s", expected, got);                                    \
+        fprintf(stderr, "Expected %s but got %s", expected, got);                           \
         exit(EXIT_FAILURE);                                                                 \
     } else if ((got) != NULL && (expected) != NULL) {                                       \
         if (strcmp(got, expected) != 0) {                                                   \
             PRINT_ASSERT_ERR();                                                             \
-            printf("Expected %s but got %s", expected, got);                                \
+            fprintf(stderr, "Expected %s but got %s", expected, got);                       \
             exit(EXIT_FAILURE);                                                             \
         }                                                                                   \
     }                                                                                       \
 } while (0)
 
 
-#define assert_token_type(got, expected)                                                \
-do {                                                                                    \
-    if ((got) != (expected)) {                                                          \
-        PRINT_ASSERT_ERR();                                                             \
-        printf("Expected %s but got %s", get_type_str(expected), get_type_str(got));    \
-        exit(EXIT_FAILURE);                                                             \
-    }                                                                                   \
+#define assert_token_type(got, expected)                                                        \
+do {                                                                                            \
+    if ((got) != (expected)) {                                                                  \
+        PRINT_ASSERT_ERR();                                                                     \
+        fprintf(stderr, "Expected %s but got %s", get_type_str(expected), get_type_str(got));   \
+        exit(EXIT_FAILURE);                                                                     \
+    }                                                                                           \
 } while (0)
 
 
-#define assert_not_null(got)                    \
-do {                                            \
-    if ((got) == NULL) {                        \
-        PRINT_ASSERT_ERR();                     \
-        printf("Expected non null pointer");    \
-        exit(EXIT_FAILURE);                     \
-    }                                           \
+#define assert_not_null(got)                            \
+do {                                                    \
+    if ((got) == NULL) {                                \
+        PRINT_ASSERT_ERR();                             \
+        fprintf(stderr, "Expected non null pointer");   \
+        exit(EXIT_FAILURE);                             \
+    }                                                   \
 } while (0)
 
 
-#define assert_error(got, expected)                                                                 \
-do {                                                                                                \
-    if ((got) != (expected)) {                                                                      \
-        PRINT_ASSERT_ERR();                                                                         \
-        printf("Expected %s but got %s", get_error_type_str(expected), get_error_type_str(got));    \
-        exit(EXIT_FAILURE);                                                                         \
-    }                                                                                               \
+#define assert_error(got, expected)                                                                         \
+do {                                                                                                        \
+    if ((got) != (expected)) {                                                                              \
+        PRINT_ASSERT_ERR();                                                                                 \
+        fprintf(stderr, "Expected %s but got %s", get_error_type_str(expected), get_error_type_str(got));   \
+        exit(EXIT_FAILURE);                                                                                 \
+    }                                                                                                       \
 } while (0)
 
 #endif
