9814947f1be6f1fd6b78293da50796d6e587c13a
Koehn <bfj.koehn@gmail.com>
2021-12-17 20:08:32 +0100

    indented assert macro definitions

--- test/test_asserts.h
+++ test/test_asserts.h
@@ -13,55 +13,55 @@
     exit(EXIT_FAILURE)
 
 
-#define ASSERT_INT(got, expected)                                   \
-do {                                                                \
-    if ((got) != (expected)) {                                      \
-        PRINT_ASSERT_ERR("Expected %d but got %d", expected, got);  \
-    }                                                               \
-} while (0)
+#define ASSERT_INT(got, expected)                                       \
+    do {                                                                \
+        if ((got) != (expected)) {                                      \
+            PRINT_ASSERT_ERR("Expected %d but got %d", expected, got);  \
+        }                                                               \
+    } while (0)
 
 
-#define ASSERT_SIZE_T(got, expected)                                    \
-do {                                                                    \
-    if ((got) != (expected)) {                                          \
-        PRINT_ASSERT_ERR("Expected %zu but got %zu", expected, got);    \
-    }                                                                   \
-} while (0)
+#define ASSERT_SIZE_T(got, expected)                                        \
+    do {                                                                    \
+        if ((got) != (expected)) {                                          \
+            PRINT_ASSERT_ERR("Expected %zu but got %zu", expected, got);    \
+        }                                                                   \
+    } while (0)
 
 
-#define ASSERT_STR(got, expected)                                                           \
-do {                                                                                        \
-    if (((expected) == NULL && (got) != NULL) || ((got) == NULL && (expected) != NULL)) {   \
-        PRINT_ASSERT_ERR("Expected %s but got %s", expected, got);                          \
-    } else if ((got) != NULL && (expected) != NULL) {                                       \
-        if (strcmp(got, expected) != 0) {                                                   \
-            PRINT_ASSERT_ERR("Expected %s but got %s", expected, got);                      \
-        }                                                                                   \
-    }                                                                                       \
-} while (0)
+#define ASSERT_STR(got, expected)                                                               \
+    do {                                                                                        \
+        if (((expected) == NULL && (got) != NULL) || ((got) == NULL && (expected) != NULL)) {   \
+            PRINT_ASSERT_ERR("Expected %s but got %s", expected, got);                          \
+        } else if ((got) != NULL && (expected) != NULL) {                                       \
+            if (strcmp(got, expected) != 0) {                                                   \
+                PRINT_ASSERT_ERR("Expected %s but got %s", expected, got);                      \
+            }                                                                                   \
+        }                                                                                       \
+    } while (0)
 
 
-#define assert_token_type(got, expected)                                                        \
-do {                                                                                            \
-    if ((got) != (expected)) {                                                                  \
-        PRINT_ASSERT_ERR("Expected %s but got %s", get_type_str(expected), get_type_str(got));  \
-    }                                                                                           \
-} while (0)
+#define assert_token_type(got, expected)                                                            \
+    do {                                                                                            \
+        if ((got) != (expected)) {                                                                  \
+            PRINT_ASSERT_ERR("Expected %s but got %s", get_type_str(expected), get_type_str(got));  \
+        }                                                                                           \
+    } while (0)
 
 
-#define ASSERT_NOT_NULL(got)                                                    \
-do {                                                                            \
-    if ((got) == NULL) {                                                        \
-        PRINT_ASSERT_ERR("Expected non null pointer, but got %p", (void*)got);  \
-    }                                                                           \
-} while (0)
+#define ASSERT_NOT_NULL(got)                                                        \
+    do {                                                                            \
+        if ((got) == NULL) {                                                        \
+            PRINT_ASSERT_ERR("Expected non null pointer, but got %p", (void*)got);  \
+        }                                                                           \
+    } while (0)
 
 
-#define ASSERT_ERROR(got, expected)                                                                         \
-do {                                                                                                        \
-    if ((got) != (expected)) {                                                                              \
-        PRINT_ASSERT_ERR("Expected %s but got %s", get_error_type_str(expected), get_error_type_str(got));  \
-    }                                                                                                       \
-} while (0)
+#define ASSERT_ERROR(got, expected)                                                                             \
+    do {                                                                                                        \
+        if ((got) != (expected)) {                                                                              \
+            PRINT_ASSERT_ERR("Expected %s but got %s", get_error_type_str(expected), get_error_type_str(got));  \
+        }                                                                                                       \
+    } while (0)
 
 #endif
