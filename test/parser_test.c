90e97bee5ebb5624bb59cf8161d37c8fd42455b3
Koehn <bfj.koehn@gmail.com>
2021-12-20 16:28:50 +0100

    Added file parser_test() and made very basic tests for primary_expr

---
+++ test/parser_test.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+#include "token.h"
+#include "tokenizer.h"
+#include "util.h"
+
+#include "ast/translation_unit.h"
+
+#include "test_asserts.h"
+
+static void primary_expr_test();
+
+void parser_test() {
+    primary_expr_test();
+    printf("Parser test successful\n");
+}
+
+static void test_primary_expr_identifier(const char* spell) {
+    struct token* tokens = tokenize(spell, "a string");
+
+    struct parser_state s = {.it = tokens};
+
+    struct primary_expr* res = parse_primary_expr(&s);
+    ASSERT_NOT_NULL(res);
+    ASSERT_TOKEN_TYPE(s.it->type, INVALID);
+
+    ASSERT(res->type == PRIMARY_EXPR_IDENTIFIER);
+    ASSERT_STR(res->identifier->spelling, spell);
+
+    ASSERT_NULL(tokens[0].spelling);
+
+    free_primary_expr(res);
+    free_tokenizer_result(tokens);
+}
+
+static void test_primary_expr_constant(enum token_type type, const char* spell) {
+    struct token* tokens = tokenize(spell, "not a file so I can write whatever here");
+
+    struct parser_state s = {.it = tokens};
+
+    struct primary_expr *res = parse_primary_expr(&s);
+    ASSERT_NOT_NULL(res);
+    ASSERT_TOKEN_TYPE(s.it->type, INVALID);
+
+    ASSERT(res->type == PRIMARY_EXPR_CONSTANT);
+    ASSERT_TOKEN_TYPE(res->constant.type, type);
+    ASSERT_STR(res->constant.spelling, spell);
+
+    ASSERT_NULL(tokens[0].spelling);
+
+    free_primary_expr(res);
+    free_tokenizer_result(tokens);
+}
+
+static void test_primary_expr_string(const char* spell) {
+    struct token* tokens = tokenize(spell, "no_file.c");
+
+    struct parser_state s = {.it = tokens};
+
+    struct primary_expr *res = parse_primary_expr(&s);
+    ASSERT_NOT_NULL(res);
+    ASSERT_TOKEN_TYPE(s.it->type, INVALID);
+
+    ASSERT(res->type == PRIMARY_EXPR_STRING_LITERAL);
+    ASSERT(res->string.is_func == false);
+    ASSERT_STR(res->string.lit.spelling, spell);
+
+    free_primary_expr(res);
+    free_tokenizer_result(tokens);
+}
+
+static void primary_expr_test() {
+    test_primary_expr_constant(F_CONSTANT, "3.13234324");
+    test_primary_expr_constant(I_CONSTANT, "32575");
+    test_primary_expr_identifier("super_cool_identifier");
+    test_primary_expr_identifier("another_cool_identifier");
+    test_primary_expr_string("\"Test string it does not matter whether this is an actual string literal but hey\"");
+}
--- test/test_main.c
+++ test/test_main.c
@@ -4,8 +4,11 @@ extern void error_test();
 
 extern void tokenizer_test();
 
+extern void parser_test();
+
 int main() {
     error_test();
     tokenizer_test();
+    parser_test();
     printf("All tests successful\n");
 }
