c1b1a0feb6794afacdcea3850074cb6c00a7e193
Koehn <bfj.koehn@gmail.com>
2022-01-20 12:38:18 +0100

    Removed include for error.h and added assert() that tokens is non-null also removed assert that checks whether parser got to end of file, as this will not be the case when there is an error

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -2,13 +2,12 @@
 
 #include <assert.h>
 
-#include "error.h"
-
 #include "parser/parser_state.h"
 
 struct translation_unit parse_tokens(struct token* tokens) {
+    assert(tokens);
+
     struct parser_state state = {.it = tokens};
     struct translation_unit res = parse_translation_unit(&state);
-    assert(state.it->type == INVALID);
     return res;
 }
