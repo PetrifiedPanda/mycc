d772c01226681a98786e7319f7e8e3993079aa64
Koehn <bfj.koehn@gmail.com>
2022-01-24 20:05:49 +0100

    Implemented parse_pointer()

--- include/ast/pointer.h
+++ include/ast/pointer.h
@@ -3,6 +3,8 @@
 
 #include <stddef.h>
 
+#include "parser/parser_state.h"
+
 struct type_qual_list;
 
 struct pointer {
@@ -10,7 +12,7 @@ struct pointer {
     struct type_qual_list* quals_after_ptr;
 };
 
-struct pointer* create_pointer(struct type_qual_list* quals_after_ptr, size_t num_indirs);
+struct pointer* parse_pointer(struct parser_state* s);
 
 void free_pointer(struct pointer* p);
 
--- src/ast/pointer.c
+++ src/ast/pointer.c
@@ -5,11 +5,49 @@
 
 #include "util.h"
 
-struct pointer* create_pointer(struct type_qual_list* quals_after_ptr, size_t num_indirs) {
-    assert(num_indirs > 0);
+struct pointer* parse_pointer(struct parser_state* s) {
+    if (!accept(s, ASTERISK)) {
+        return NULL;
+    }
+
     struct pointer* res = xmalloc(sizeof(struct pointer));
-    res->num_indirs = num_indirs;
-    res->quals_after_ptr = quals_after_ptr;
+    res->num_indirs = 1;
+    res->quals_after_ptr = xmalloc(sizeof(struct type_qual_list));
+
+    if (is_type_qual(s->it->type)) {
+        res->quals_after_ptr[0] = parse_type_qual_list(s);
+        if (res->quals_after_ptr[0].len == 0) {
+            free(res->quals_after_ptr);
+            free(res);
+            return NULL;
+        }
+    } else {
+        res->quals_after_ptr[0] = (struct type_qual_list){.len = 0, .type_quals = NULL};
+    }
+
+    size_t alloc_size = res->num_indirs;
+    while (s->it->type == ASTERISK) {
+        accept_it(s);
+
+        if (res->num_indirs == alloc_size) {
+            grow_alloc((void**)&res->quals_after_ptr, &alloc_size, sizeof(struct type_qual_list));
+        }
+
+        if (is_type_qual(s->it->type)) {
+            res->quals_after_ptr[res->num_indirs] = parse_type_qual_list(s);
+            if (res->quals_after_ptr[res->num_indirs].len == 0) {
+                free_pointer(res);
+                return NULL;
+            }
+        } else {
+            res->quals_after_ptr[0] = (struct type_qual_list){.len = 0, .type_quals = NULL};
+        }
+
+        ++res->num_indirs;
+    }
+
+    res->quals_after_ptr = xrealloc(res->quals_after_ptr, sizeof(struct type_qual_list) * res->num_indirs);
+
     return res;
 }
 
