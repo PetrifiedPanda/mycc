261eafe0a2559883bf154343f1258843b1dc2753
Koehn <bfj.koehn@gmail.com>
2022-01-24 19:49:42 +0100

    Added code skeleton for parse_param_type_list()

--- include/ast/param_type_list.h
+++ include/ast/param_type_list.h
@@ -3,6 +3,8 @@
 
 #include <stdbool.h>
 
+#include "parser/parser_state.h"
+
 struct param_list;
 
 struct param_type_list {
@@ -10,7 +12,7 @@ struct param_type_list {
     struct param_list* param_list;
 };
 
-struct param_type_list create_param_type_list(bool is_variadic, struct param_list* param_list);
+struct param_type_list parse_param_type_list(struct parser_state* s);
 
 void free_param_type_list(struct param_type_list* l);
 
--- src/ast/param_type_list.c
+++ src/ast/param_type_list.c
@@ -2,9 +2,18 @@
 
 #include <assert.h>
 
-struct param_type_list create_param_type_list(bool is_variadic, struct param_list* param_list) {
+static struct param_type_list create_param_type_list(bool is_variadic, struct param_list* param_list) {
     assert(param_list);
     return (struct param_type_list){.is_variadic = is_variadic, .param_list = param_list};
+}
+
+struct param_type_list parse_param_type_list(struct parser_state* s) {
+    (void)s;
+    // TODO:
+    return (struct param_type_list) {
+        .is_variadic = false,
+        .param_list = NULL
+    };
 }
 
 void free_param_type_list(struct param_type_list* l) {
