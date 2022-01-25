174727f29ff6201105fa4111c716570a0a90304e
Koehn <bfj.koehn@gmail.com>
2022-01-25 13:52:48 +0100

    Implemented parse_struct_declarator()

--- include/ast/struct_declarator.h
+++ include/ast/struct_declarator.h
@@ -1,6 +1,8 @@
 #ifndef STRUCT_DECLARATOR_H
 #define STRUCT_DECLARATOR_H
 
+#include "parser/parser_state.h"
+
 struct declarator;
 struct const_expr;
 
@@ -9,7 +11,7 @@ struct struct_declarator {
     struct const_expr* bit_field;
 };
 
-struct struct_declarator* create_struct_declarator(struct declarator* decl, struct const_expr* bit_field);
+struct struct_declarator* parse_struct_declarator(struct parser_state* s);
 
 void free_struct_declarator_children(struct struct_declarator* d);
 void free_struct_declarator(struct struct_declarator* d);
--- src/ast/struct_declarator.c
+++ src/ast/struct_declarator.c
@@ -1,16 +1,38 @@
 #include "ast/struct_declarator.h"
 
 #include <stdlib.h>
-#include <assert.h>
 
+#include "error.h"
 #include "util.h"
 
-struct struct_declarator* create_struct_declarator(struct declarator* decl, struct const_expr* bit_field) {
-    assert(decl || bit_field);
+struct struct_declarator* parse_struct_declarator(struct parser_state* s) {
     struct struct_declarator* res = xmalloc(sizeof(struct struct_declarator));
-    res->decl = decl;
-    res->bit_field = bit_field;
-    
+
+    if (s->it->type != COLON) {
+        res->decl = parse_declarator(s);
+        if (!res->decl) {
+            free(res);
+            return NULL;
+        }
+    } else {
+        res->decl = NULL;
+    }
+
+    if (s->it->type == COLON) {
+        accept_it(s);
+        res->bit_field = parse_const_expr(s);
+        if (!res->bit_field) {
+            free_struct_declarator(res);
+            return NULL;
+        }
+    } else {
+        res->bit_field = NULL;
+        if (!res->decl) {
+            set_error_file(ERR_PARSER, s->it->file, s->it->source_loc, "Expected a declarator or a bit field specifier");
+            return NULL;
+        }
+    }
+
     return res;
 }
 
