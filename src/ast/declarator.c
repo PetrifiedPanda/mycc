bf2df97c5767388d661125909960cfa8a36b767e
Koehn <bfj.koehn@gmail.com>
2022-01-25 13:42:35 +0100

    Implemented parse_declarator()

--- src/ast/declarator.c
+++ src/ast/declarator.c
@@ -1,23 +1,31 @@
 #include "ast/declarator.h"
 
 #include <stdlib.h>
-#include <assert.h>
 
 #include "util.h"
 
-static struct declarator* create_declarator(struct pointer* ptr, struct direct_declarator* direct_decl) {
-    assert(direct_decl);
-    struct declarator* res = xmalloc(sizeof(struct declarator));
-    res->ptr = ptr;
-    res->direct_decl = direct_decl;
-    
-    return res;
-}
-
 struct declarator* parse_declarator(struct parser_state* s) {
-    (void)s;
-    // TODO:
-    return NULL;
+    struct declarator* res = xmalloc(sizeof(struct declarator));
+    if (s->it->type == ASTERISK) {
+        res->ptr = parse_pointer(s);
+        if (!res->ptr) {
+            free(res);
+            return NULL;
+        }
+    } else {
+        res->ptr = NULL;
+    }
+
+    res->direct_decl = parse_direct_declarator(s);
+    if (!res->direct_decl) {
+        if (res->ptr) {
+            free_pointer(res->ptr);
+        }
+        free(res);
+        return NULL;
+    }
+
+    return res;
 }
 
 static void free_children(struct declarator* d) {
