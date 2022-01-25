ca28cd401b6a19abbc9c32af54361db9fd69e5b7
Koehn <bfj.koehn@gmail.com>
2022-01-25 14:04:23 +0100

    Implemented parse_struct_declarator_list()

--- include/ast/struct_declarator_list.h
+++ include/ast/struct_declarator_list.h
@@ -3,6 +3,8 @@
 
 #include <stddef.h>
 
+#include "parser/parser_state.h"
+
 struct struct_declarator;
 
 struct struct_declarator_list {
@@ -10,7 +12,7 @@ struct struct_declarator_list {
     struct struct_declarator* decls;
 };
 
-struct struct_declarator_list create_struct_declarator_list(struct struct_declarator* decls, size_t len);
+struct struct_declarator_list parse_struct_declarator_list(struct parser_state* s);
 
 void free_struct_declarator_list(struct struct_declarator_list* l);
 
--- src/ast/struct_declarator_list.c
+++ src/ast/struct_declarator_list.c
@@ -1,15 +1,44 @@
 #include "ast/struct_declarator_list.h"
 
 #include <stdlib.h>
-#include <assert.h>
 
-struct struct_declarator_list create_struct_declarator_list(struct struct_declarator* decls, size_t len) {
-    if (len > 0) {
-        assert(decls);
-    } else {
-        assert(decls == NULL);
+#include "util.h"
+
+struct struct_declarator_list parse_struct_declarator_list(struct parser_state* s) {
+    struct struct_declarator_list res = {
+        .len = 1,
+        .decls = xmalloc(sizeof(struct struct_declarator_list))
+    };
+
+    if (!parse_struct_declarator_inplace(s, &res.decls[0])) {
+        free(res.decls);
+        return (struct struct_declarator_list) {
+            .len = 0,
+            .decls = NULL
+        };
     }
-    return (struct struct_declarator_list){.len = len, .decls = decls};
+
+    size_t alloc_len = res.len;
+    while (s->it->type == COMMA) {
+        accept_it(s);
+        if (res.len == alloc_len) {
+            grow_alloc((void**)&res.decls, &alloc_len, sizeof(struct struct_declarator));
+        }
+
+        if (!parse_struct_declarator_inplace(s, &res.decls[res.len])) {
+            free_struct_declarator_list(&res);
+            return (struct struct_declarator_list) {
+                    .len = 0,
+                    .decls = NULL
+            };
+        }
+
+        ++res.len;
+    }
+
+    res.decls = xrealloc(res.decls, sizeof(struct struct_declarator) * res.len);
+
+    return res;
 }
 
 void free_struct_declarator_list(struct struct_declarator_list* l) {
