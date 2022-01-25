92f12897b7844237f15deac4f74991288d63dec9
Koehn <bfj.koehn@gmail.com>
2022-01-25 20:32:46 +0100

    implemented parse_param_list()

--- src/ast/param_list.c
+++ src/ast/param_list.c
@@ -1,24 +1,39 @@
 #include "ast/param_list.h"
 
 #include <stdlib.h>
-#include <assert.h>
 
 #include "util.h"
 
-static struct param_list* create_param_list(struct param_declaration* decls, size_t len) {
-    assert(len > 0);
-    assert(decls);
-    struct param_list* res = xmalloc(sizeof(struct param_list));
-    res->len = len;
-    res->decls = decls;
-    
-    return res;
-}
-
 struct param_list* parse_param_list(struct parser_state* s) {
-    (void)s;
-    // TODO:
-    return NULL;
+    struct param_list* res = xmalloc(sizeof(struct param_list));
+    res->decls = xmalloc(sizeof(struct param_declaration));
+    res->len = 1;
+
+    if (!parse_param_declaration_inplace(s, &res->decls[0])) {
+        free(res->decls);
+        free(res);
+        return NULL;
+    }
+
+    size_t alloc_len = res->len;
+    while (s->it->type == COMMA && s->it[1].type != ELLIPSIS) {
+        accept_it(s);
+
+        if (res->len == alloc_len) {
+            grow_alloc((void**)&res->decls, &alloc_len, sizeof(struct param_declaration));
+        }
+
+        if (!parse_param_declaration_inplace(s, &res->decls[res->len])) {
+            free_param_list(res);
+            return NULL;
+        }
+
+        ++res->len;
+    }
+
+    res->decls = xrealloc(res->decls, sizeof(struct param_declaration) * res->len);
+
+    return res;
 }
 
 static void free_children(struct param_list* l) {
