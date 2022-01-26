57a82828fa81668e185cf6a21569fa1c41c888c8
Koehn <bfj.koehn@gmail.com>
2022-01-26 16:05:09 +0100

    removed unneeded if

--- src/ast/func_def.c
+++ src/ast/func_def.c
@@ -1,11 +1,7 @@
 #include "ast/func_def.h"
 
-#include <stdlib.h>
-
 void free_func_def_children(struct func_def* d) {
-    if (d->specs) {
-        free_declaration_specs(d->specs);
-    }
+    free_declaration_specs(d->specs);
     free_declarator(d->decl);
     free_declaration_list(&d->decl_list);
     free_compound_statement(d->comp);
