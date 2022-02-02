0e58d7c8a579964b4bedce8fe24d7896e19d1cbe
Koehn <bfj.koehn@gmail.com>
2022-02-02 10:52:13 +0100

    Fixed memory leak

--- src/ast/atomic_type_spec.c
+++ src/ast/atomic_type_spec.c
@@ -30,4 +30,5 @@ struct atomic_type_spec* parse_atomic_type_spec(struct parser_state* s) {
 
 void free_atomic_type_spec(struct atomic_type_spec* s) {
     free_type_name(s->type_name);
+    free(s);
 }
