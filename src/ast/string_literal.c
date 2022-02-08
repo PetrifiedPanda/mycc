085195f9b09930f5dc2b319a085f8b530bf29731
Koehn <bfj.koehn@gmail.com>
2022-02-08 15:54:30 +0100

    Removed unused function

--- src/ast/string_literal.c
+++ src/ast/string_literal.c
@@ -1,12 +1,6 @@
 #include "ast/string_literal.h"
 
 #include <stdlib.h>
-#include <assert.h>
-
-struct string_literal create_string_literal(char* spelling) {
-    assert(spelling);
-    return (struct string_literal){.spelling = spelling};
-}
 
 void free_string_literal(struct string_literal* l) {
     free(l->spelling);
