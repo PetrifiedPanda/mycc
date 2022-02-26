82f1e95144bf0a5b16279e2f6082c7b58c8e6ed9
Koehn <bfj.koehn@gmail.com>
2022-02-26 21:10:18 +0100

    Added ability for main to be called with file as parameter and tokenize and parse the contents of the given file

--- main.c
+++ main.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "tokenizer.h"
-#include "parser/parser.h"
 #include "error.h"
+#include "util.h"
 
-int main() {
-    const char*
-        code = "typedef struct typedeftest /* This is a comment \n"
-               "that goes over\n"
-               "multiple lines\n"
-               "*/\n"
-               "{\n"
-               "\tlong int* n;\n"
-               "const long double *m;\n"
-               "} Typedeftest; // Line comment\n"
-               "const char* lstr = \n"
-               "L\"Long string literal to check if long strings work\";\n"
-               "int n = 0x123213 + 132 << 32 >> 0x123 - 0123 / 12;\n"
-               "const char* str = \"Normal string literal\";\n"
-               "int arr[1 ? 100 : 1000];\n";
-    printf("Tokenizing...\n");
-    struct token* tokens = tokenize(code, "this_is_not_an_actual_file.c");
+#include "parser/parser.h"
+
+static void check_error();
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        fprintf(stderr, "%s: no input file\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    const char* filename = argv[1];
+
+    char* file_contents = read_file(filename);
+    assert(file_contents);
+
+    struct token* tokens = tokenize(file_contents, filename);
+    check_error();
+    
+    struct translation_unit tl = parse_tokens(tokens);
+    check_error();
+    
+    printf("Finished tokenization successfully\n");
+
+    free_translation_unit(&tl);
+    free_tokenizer_result(tokens);
+    free(file_contents);
+}
+
+static void check_error() {
     if (get_last_error() != ERR_NONE) {
         fprintf(stderr, "%s\n", get_error_string());
-        clear_last_error();
-    } else {
-        printf("Tokenizer finished successfully\n");
+        exit(EXIT_FAILURE);
     }
-
-    for (const struct token* it = tokens; it->type != INVALID; ++it) {
-        printf("Type: %s, Spelling: %s, line: %zu, idx: %zu\n",
-               get_type_str(it->type),
-               it->spelling,
-               it->source_loc.line,
-               it->source_loc.index);
-    }
-
-    free_tokenizer_result(tokens);
-    printf("Finished\n");
 }
+
