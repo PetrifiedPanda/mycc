2640538e5e30dc40dc69d3b63f86a08af7b8d710
Koehn <bfj.koehn@gmail.com>
2022-03-21 02:02:56 +0100

    put preproc_macro.h in a state that will compile should it be included

--- include/preproc/preproc_macro.h
+++ include/preproc/preproc_macro.h
@@ -1,24 +1,7 @@
 #ifndef PREPROC_MACRO_H
 #define PREPROC_MACRO_H
 
-#include <stddef.h>
-
-#include "preproc/preproc_token.h"
-
-struct str_and_arg {
-    char* spelling;
-    size_t arg_num;
-};
-
-struct preproc_macro {
-    char* spelling;
-    size_t num_args;
-
-    size_t len;
-    struct str_and_arg* expansion;
-};
-
-void expand_macro(struct preproc_macro* m, const char** args, size_t num_args);
+// TODO: implement macro expansion
 
 #endif
 
