f583acfaa90644198d50b18264117d1cbbdc3963
Koehn <bfj.koehn@gmail.com>
2022-03-31 16:36:56 +0200

    Removed old definitions of init_token functions

--- src/token.c
+++ src/token.c
@@ -32,35 +32,6 @@ struct token create_token_copy(enum token_type type,
     return create_token(type, alloc_string_copy(spelling), loc, filename); 
 }
 
-void init_token(struct token* t,
-                enum token_type type,
-                char* spelling,
-                struct source_location loc,
-                const char* filename) {
-    assert(t);
-    assert(filename);
-
-    if (get_spelling(type) == NULL) {
-        assert(spelling);
-        t->spelling = spelling;
-    } else {
-        assert(spelling == NULL);
-        t->spelling = NULL;
-    }
-
-    t->file = alloc_string_copy(filename);
-    t->type = type;
-    t->source_loc = loc;
-}
-
-void init_token_copy(struct token* t,
-                     enum token_type type,
-                     const char* spelling,
-                     struct source_location loc,
-                     const char* filename) {
-    init_token(t, type, alloc_string_copy(spelling), loc, filename);
-}
-
 void free_token(struct token* t) {
     assert(t);
     free(t->spelling);
