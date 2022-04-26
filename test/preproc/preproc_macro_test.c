00812963a19f3eee7ed35eeb8ee341afc5ac129a
Koehn <bfj.koehn@gmail.com>
2022-04-26 19:14:07 +0200

    Made preproc macro tests easier to make by using the fact that preproc() now returns preprocessor tokens. This resulted in the tests now not testing the source location and file of the token

--- test/preproc/preproc_macro_test.c
+++ test/preproc/preproc_macro_test.c
@@ -1,20 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "token.h"
 #include "token_type.h"
 #include "util.h"
 
+#include "preproc/preproc.h"
 #include "preproc/preproc_macro.h"
 
 #include "../test_asserts.h"
 
-static void check_token(const struct token* got,
-                        const struct token* ex) {
-    ASSERT_TOKEN_TYPE(got->type, ex->type);
-    ASSERT_STR(got->spelling, ex->spelling);
-    ASSERT_STR(got->file, ex->file);
-    ASSERT_SIZE_T(got->source_loc.line, ex->source_loc.line);
-    ASSERT_SIZE_T(got->source_loc.index, ex->source_loc.index);
+static size_t get_tokens_len(const struct token* tokens) {
+    size_t len = 0;
+    const struct token* it = tokens;
+
+    while (it->type != INVALID) {
+        ++len;
+        ++it;
+    }
+
+    return len;
+}
+
+static void test_preproc_macro(const struct preproc_macro* macro, size_t macro_idx, const char* input, const char* output) {
+    struct token* tokens = preproc_string(input, "source_file.c");
+    const size_t tokens_len = get_tokens_len(tokens);
+
+    struct preproc_state state = {
+        .len = tokens_len,
+        .cap = tokens_len,
+        .tokens = tokens,
+    };
+
+    ASSERT(expand_preproc_macro(&state, macro, macro_idx, NULL));
+
+    struct token* expected = preproc_string(output, "source_file.c");
+    ASSERT_SIZE_T(state.len, get_tokens_len(expected));
+
+    for (size_t i = 0; i < state.len; ++i) {
+        ASSERT_TOKEN_TYPE(state.tokens[i].type, expected[i].type);
+        ASSERT_STR(state.tokens[i].spelling, expected[i].spelling);
+        free_token(&state.tokens[i]);
+    }
+    free(state.tokens);
+    free_tokens(expected);
 }
 
 TEST(object_like) {
@@ -35,62 +64,9 @@ TEST(object_like) {
         .expansion = expansion,
     };
 
-    char* file = (char*)"file.c";
-
-    // int var = MACRO;
-    // func();
-    const struct token tokens[] = {
-        {IDENTIFIER, "int", file, {1, 5}},
-        {IDENTIFIER, "var", file, {1, 9}},
-        {ASSIGN, NULL, file, {1, 13}},
-        {IDENTIFIER,
-         alloc_string_copy(macro.spelling),
-         alloc_string_copy(file),
-         {1, 15}}, // This is the only token freed by the function
-        {SEMICOLON, NULL, file, {1, 20}},
-        {IDENTIFIER, "func", file, {2, 5}},
-        {LBRACKET, NULL, file, {2, 10}},
-        {RBRACKET, NULL, file, {2, 11}},
-        {SEMICOLON, NULL, file, {2, 12}},
-    };
-
-    enum { TOKENS_LEN = sizeof(tokens) / sizeof(struct token) };
-
-    struct preproc_state state = {
-        .len = TOKENS_LEN,
-        .cap = TOKENS_LEN,
-        .tokens = malloc(sizeof(tokens)),
-    };
-    ASSERT_NOT_NULL(state.tokens);
-
-    memcpy(state.tokens, tokens, sizeof(tokens));
-
-    const size_t prev_len = state.len;
-    ASSERT(expand_preproc_macro(&state, &macro, 3, NULL));
-    ASSERT_SIZE_T(state.len, prev_len + EXP_LEN - 1);
-    ASSERT_SIZE_T(state.len, state.cap);
-
-    for (size_t i = 0; i < 3; ++i) {
-        const struct token* ex = &tokens[i];
-        const struct token* got = &state.tokens[i];
-
-        check_token(got, ex);
-    }
-
-    for (size_t i = 0; i < macro.expansion_len; ++i) {
-        struct token* ex = &expansion[i].token;
-        struct token* got = &state.tokens[i + 3];
-
-        check_token(got, ex);
-        // only the inserted functions have heap allocated strings
-        free_token(got);
-    }
-
-    for (size_t i = 0; i < TOKENS_LEN - 4; ++i) {
-        check_token(&state.tokens[3 + macro.expansion_len + i], &tokens[4 + i]);
-    }
-
-    free(state.tokens);
+    test_preproc_macro(&macro, 3, "int var = MACRO;\nfunc();", "int var = 1 + 2;\nfunc();");
+    test_preproc_macro(&macro, 0, "MACRO; for (size_t i = 0; i < 42; ++i) continue;", "1 + 2; for (size_t i = 0; i < 42; ++i) continue;");
+    test_preproc_macro(&macro, 5, "int x = 1000; MACRO", "int x = 1000; 1 + 2");
 }
 
 TEST(object_like_empty) {
@@ -105,52 +81,9 @@ TEST(object_like_empty) {
         .expansion = NULL,
     };
     
-    char* file = (char*)"not_a_file.c";
-
-    // function EMPTY_MACRO (var, 2);
-    const struct token tokens[] = {
-        {IDENTIFIER, "function", file, {1, 1}},
-        {IDENTIFIER, alloc_string_copy(macro_name), alloc_string_copy(file), {1, 9}},
-        {LBRACKET, NULL, file, {1, 21}},
-        {IDENTIFIER, "var", file, {1, 22}},
-        {COMMA, NULL, file, {1, 25}},
-        {I_CONSTANT, "2", file, {1, 25}},
-        {RBRACKET, NULL, file, {1, 26}},
-        {SEMICOLON, NULL, file, {1, 27}},
-    };
-    enum { TOKENS_LEN = sizeof(tokens) / sizeof(struct token) };
-    
-    struct preproc_state s = {
-        .len = TOKENS_LEN,
-        .cap = TOKENS_LEN,
-        .tokens = malloc(sizeof(tokens)),
-    };
-
-    memcpy(s.tokens, tokens, sizeof(tokens));
-
-    const struct token result[] = {
-        {IDENTIFIER, "function", file, {1, 1}},
-        {LBRACKET, NULL, file, {1, 21}},
-        {IDENTIFIER, "var", file, {1, 22}},
-        {COMMA, NULL, file, {1, 25}},
-        {I_CONSTANT, "2", file, {1, 25}},
-        {RBRACKET, NULL, file, {1, 26}},
-        {SEMICOLON, NULL, file, {1, 27}},
-    };
-    enum { RESULT_LEN = sizeof(result) / sizeof(struct token) };
-
-    const size_t prev_len = s.len;
-    ASSERT(expand_preproc_macro(&s, &macro, 1, NULL));
-    ASSERT_SIZE_T(s.len, prev_len - 1);
-    
-    for (size_t i = 0; i < RESULT_LEN; ++i) {
-        const struct token* got = &s.tokens[i];
-        const struct token* ex = &result[i];
-
-        check_token(got, ex);
-    }
-
-    free(s.tokens);
+    test_preproc_macro(&macro, 1, "function EMPTY_MACRO (var, 2);", "function (var, 2);");
+    test_preproc_macro(&macro, 0, "EMPTY_MACRO int n = 1000;", "int n = 1000;");
+    test_preproc_macro(&macro, 10, "while (true) x *= 2 * 2;\nEMPTY_MACRO;", "while (true) x *= 2 * 2;\n;");
 }
 
 TEST_SUITE_BEGIN(preproc_macro, 2) {
