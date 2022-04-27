e84ac065c16623a3d50b119fc502d492ab6c2a61
Koehn <bfj.koehn@gmail.com>
2022-04-27 21:40:14 +0200

    Added asserts to helper functions

--- test/test_helpers.c
+++ test/test_helpers.c
@@ -2,14 +2,20 @@
 
 #include "preproc/preproc.h"
 
+#include "test_asserts.h"
+
 struct token* tokenize(const char* file) {
     struct token* res = preproc(file);
+    ASSERT_NOT_NULL(res);
+    ASSERT_NO_ERROR();
     convert_preproc_tokens(res);
     return res;
 }
 
 struct token* tokenize_string(const char* str, const char* file) {
     struct token* res = preproc_string(str, file);
+    ASSERT_NOT_NULL(res);
+    ASSERT_NO_ERROR();
     convert_preproc_tokens(res);
     return res;
 }
