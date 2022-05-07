a79ebb9920e4800a5a0b605c74e44bf96df0f1c5
Koehn <bfj.koehn@gmail.com>
2022-05-07 13:55:38 +0200

    Added skeleton of function to get macro with spelling from preproc stat

--- include/preproc/preproc_state.h
+++ include/preproc/preproc_state.h
@@ -8,5 +8,11 @@ struct preproc_state {
     struct token* tokens;
 };
 
+struct preproc_macro;
+
+struct preproc_macro* find_preproc_macro(const char* spelling);
+
+#include "preproc/preproc_macro.h"
+
 #endif
 
---
+++ src/preproc/preproc_state.c
@@ -0,0 +1,7 @@
+#include "preproc/preproc_state.h"
+
+struct preproc_macro* find_preproc_macro(const char* spelling) {
+    // TODO: implement
+    (void)spelling;
+    return NULL;
+}
