5c5de0e2d02e1cc5d46aaa373a84e3ffae47cf28
Koehn <bfj.koehn@gmail.com>
2022-05-21 15:14:36 +0200

    removed assignment in one line

--- src/ast/expr/and_expr.c
+++ src/ast/expr/and_expr.c
@@ -10,7 +10,8 @@
 static bool parse_and_expr_rest(struct parser_state* s, struct and_expr* res) {
     assert(res->eq_exprs);
 
-    size_t alloc_len = res->len = 1;
+    res->len = 1;
+    size_t alloc_len = res->len;
 
     while (s->it->type == AND) {
         accept_it(s);
