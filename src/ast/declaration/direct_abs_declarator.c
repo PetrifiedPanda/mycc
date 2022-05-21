9f41efed26914fa6d61e45b801130eb80609fcc4
Koehn <bfj.koehn@gmail.com>
2022-05-21 13:59:09 +0200

    Put function suffix and array suffix in separate functions

--- src/ast/declaration/direct_abs_declarator.c
+++ src/ast/declaration/direct_abs_declarator.c
@@ -7,100 +7,108 @@
 
 static void free_abs_arr_or_func_suffix(struct abs_arr_or_func_suffix* s);
 
+static bool parse_abs_func_suffix(struct parser_state* s,
+                                  struct abs_arr_or_func_suffix* res) {
+    assert(s->it->type == LBRACKET);
+    accept_it(s);
+    res->type = ABS_ARR_OR_FUNC_SUFFIX_FUNC;
+    if (s->it->type == RBRACKET) {
+        res->func_types = (struct param_type_list){
+            .is_variadic = false,
+            .param_list = NULL,
+        };
+        accept_it(s);
+    } else {
+        res->func_types = parse_param_type_list(s);
+        if (res->func_types.param_list == NULL) {
+            return false;
+        }
+
+        if (!accept(s, RBRACKET)) {
+            free_param_type_list(&res->func_types);
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool parse_abs_arr_suffix(struct parser_state* s,
+                                 struct abs_arr_or_func_suffix* res) {
+    assert(s->it->type == LINDEX);
+    accept_it(s);
+    if (s->it->type == RINDEX) {
+        res->type = ABS_ARR_OR_FUNC_SUFFIX_ARRAY_EMPTY;
+        res->has_asterisk = false;
+        accept_it(s);
+        return true;
+    } else if (s->it->type == ASTERISK) {
+        res->type = ABS_ARR_OR_FUNC_SUFFIX_ARRAY_EMPTY;
+        res->has_asterisk = true;
+        if (!accept(s, RINDEX)) {
+            return false;
+        }
+        return true;
+    }
+
+    res->type = ABS_ARR_OR_FUNC_SUFFIX_ARRAY_DYN;
+    res->is_static = false;
+    if (s->it->type == STATIC) {
+        accept_it(s);
+        res->is_static = true;
+    }
+
+    if (is_type_qual(s->it->type)) {
+        res->type_quals = parse_type_qual_list(s);
+        if (!is_valid_type_quals(&res->type_quals)) {
+            return false;
+        }
+
+        if (s->it->type == STATIC) {
+            if (res->is_static) {
+                // TODO: maybe turn this into a warning?
+                set_parser_err(s->err,
+                               PARSER_ERR_ARR_DOUBLE_STATIC,
+                               s->it);
+                free_abs_arr_or_func_suffix(res);
+                return false;
+            } else {
+                accept_it(s);
+                res->is_static = true;
+            }
+        }
+    }
+
+    if (s->it->type == RINDEX) {
+        if (res->is_static) {
+            set_parser_err(s->err,
+                           PARSER_ERR_ARR_STATIC_NO_LEN,
+                           s->it);
+            free_abs_arr_or_func_suffix(res);
+            return false;
+        }
+
+        accept_it(s);
+    } else {
+        res->assign = parse_assign_expr(s);
+        if (!(res->assign && accept(s, RINDEX))) {
+            free_abs_arr_or_func_suffix(res);
+            return false;
+        }
+    }
+    return true;
+}
+
 static bool parse_abs_arr_or_func_suffix(struct parser_state* s,
                                          struct abs_arr_or_func_suffix* res) {
     assert(res);
     assert(s->it->type == LBRACKET || s->it->type == LINDEX);
 
     switch (s->it->type) {
-        case LBRACKET: {
-            res->type = ABS_ARR_OR_FUNC_SUFFIX_FUNC;
-            accept_it(s);
-            if (s->it->type == RBRACKET) {
-                res->func_types = (struct param_type_list){
-                    .is_variadic = false,
-                    .param_list = NULL,
-                };
-                accept_it(s);
-            } else {
-                res->func_types = parse_param_type_list(s);
-                if (res->func_types.param_list == NULL) {
-                    return false;
-                }
-
-                if (!accept(s, RBRACKET)) {
-                    free_param_type_list(&res->func_types);
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        case LINDEX: {
-            accept_it(s);
-            if (s->it->type == RINDEX) {
-                res->type = ABS_ARR_OR_FUNC_SUFFIX_ARRAY_EMPTY;
-                res->has_asterisk = false;
-                accept_it(s);
-                return true;
-            } else if (s->it->type == ASTERISK) {
-                res->type = ABS_ARR_OR_FUNC_SUFFIX_ARRAY_EMPTY;
-                res->has_asterisk = true;
-                if (!accept(s, RINDEX)) {
-                    return false;
-                }
-                return true;
-            }
-
-            res->type = ABS_ARR_OR_FUNC_SUFFIX_ARRAY_DYN;
-            res->is_static = false;
-            if (s->it->type == STATIC) {
-                accept_it(s);
-                res->is_static = true;
-            }
-
-            if (is_type_qual(s->it->type)) {
-                res->type_quals = parse_type_qual_list(s);
-                if (!is_valid_type_quals(&res->type_quals)) {
-                    return false;
-                }
-
-                if (s->it->type == STATIC) {
-                    if (res->is_static) {
-                        // TODO: maybe turn this into a warning?
-                        set_parser_err(s->err,
-                                       PARSER_ERR_ARR_DOUBLE_STATIC,
-                                       s->it);
-                        free_abs_arr_or_func_suffix(res);
-                        return false;
-                    } else {
-                        accept_it(s);
-                        res->is_static = true;
-                    }
-                }
-            }
-
-            if (s->it->type == RINDEX) {
-                if (res->is_static) {
-                    set_parser_err(s->err,
-                                   PARSER_ERR_ARR_STATIC_NO_LEN,
-                                   s->it);
-                    free_abs_arr_or_func_suffix(res);
-                    return false;
-                }
-
-                accept_it(s);
-            } else {
-                res->assign = parse_assign_expr(s);
-                if (!(res->assign && accept(s, RINDEX))) {
-                    free_abs_arr_or_func_suffix(res);
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        default: // UNREACHABLE
+        case LBRACKET:
+            return parse_abs_func_suffix(s, res);
+        case LINDEX:
+            return parse_abs_arr_suffix(s, res);
+        default:
             assert(false);
     }
 
