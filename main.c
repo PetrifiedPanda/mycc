4f7300abae55885120e5ed79cca9399dc1def297
Koehn <bfj.koehn@gmail.com>
2022-06-29 12:00:09 +0200

    made main put the resulting asts in a file with .ast appended (will probably not work on windows yet)

--- main.c
+++ main.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "preproc/preproc.h"
 
 #include "parser/parser.h"
 
 #include "ast/ast_dumper.h"
+
+#include "util/mem.h"
+
+static const char* strip_file_location(const char* filename) {
+    const char* res = filename;
+    
+    const char* it = filename;
+    while (*it != '\0') {
+        if (*it == '/') {
+            res = it + 1;
+        }
+        ++it;
+    }
+    return res;
+}
+
+static char* str_concat(const char* s1, const char* s2) {
+    const size_t len1 = strlen(s1);
+    const size_t len2 = strlen(s2);
+    const size_t len = len1 + len2;
+    char* res = xmalloc(sizeof(char) * (len + 1));
+    
+    memcpy(res, s1, len1 * sizeof(char));
+    memcpy(res + len1, s2, len2 * sizeof(char));
+    res[len] = '\0';
+    return res;
+}
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -32,8 +60,31 @@ int main(int argc, char** argv) {
             free_tokens(tokens);
             return EXIT_FAILURE;
         }
+        
+        const char* filename_only = strip_file_location(filename);
 
-        dump_ast(&tl, stdout);
+        char* out_filename = str_concat(filename_only, ".ast");
+
+        FILE* outfile = fopen(out_filename, "w");
+        if (!outfile) {
+            printf("Could not open output file %s\n", out_filename);
+            free(out_filename);
+            free_translation_unit(&tl);
+            free_tokens(tokens);
+            return EXIT_FAILURE;
+        }
+
+        dump_ast(&tl, outfile);
+
+        fflush(outfile);
+        if (fclose(outfile) != 0) {
+            printf("Failed to close output file %s\n", out_filename);
+            free(out_filename);
+            free_translation_unit(&tl);
+            free_tokens(tokens);
+            return EXIT_FAILURE;
+        }
+        free(out_filename);
 
         free_translation_unit(&tl);
         free_tokens(tokens);
