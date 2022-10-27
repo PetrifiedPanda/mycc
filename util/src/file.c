39e01e187310c96e92c0677942ff447834052f2c
Koehn <bfj.koehn@gmail.com>
2022-10-27 13:53:13 +0200

    Made non-windows-systems able to handle windows line endings

--- util/src/file.c
+++ util/src/file.c
@@ -5,6 +5,19 @@
 
 #include "util/mem.h"
 
+#ifndef _WIN32
+
+static void handle_win_line_ending(int newline_char, FILE* file) {
+    if (newline_char == '\r') {
+        int next = getc(file);
+        if (next != '\n') {
+            putc(next, file);
+        }
+    }
+}
+
+#endif
+
 void file_read_line(FILE* file,
                     char** res,
                     size_t* res_len,
@@ -18,7 +31,7 @@ void file_read_line(FILE* file,
     if (*res_len < static_buf_len) {
         *res = static_buf;
         bool copy_to_dyn_buf = false;
-        while ((c = getc(file)) != '\n' && c != EOF) {
+        while ((c = getc(file)) != '\n' && c != '\r' && c != EOF) {
             static_buf[*res_len] = (char)c;
             ++*res_len;
             if (*res_len == static_buf_len - 1) {
@@ -26,6 +39,9 @@ void file_read_line(FILE* file,
                 break;
             }
         }
+#ifndef _WIN32
+        handle_win_line_ending(c, file);
+#endif
 
         if (copy_to_dyn_buf) {
             cap = static_buf_len * 2;
@@ -43,7 +59,7 @@ void file_read_line(FILE* file,
         cap = *res_len;
     }
 
-    while ((c = getc(file)) != '\n' && c != EOF) {
+    while ((c = getc(file)) != '\n' && c != '\r' && c != EOF) {
         if (*res_len == cap) {
             grow_alloc((void**)res, &cap, sizeof(char));
         }
@@ -51,6 +67,10 @@ void file_read_line(FILE* file,
 
         ++*res_len;
     }
+
+#ifndef _WIN32
+    handle_win_line_ending(c, file);
+#endif
 
     if (*res_len == 0 && c == EOF) {
         assert(*res == NULL);
