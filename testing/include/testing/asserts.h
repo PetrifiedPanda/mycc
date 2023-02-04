e7fbfba66fc96938aae3e8f1021245e6799caba6
Benedikt <bfj.koehn@gmail.com>
2023-02-04 20:15:30 +0100

    Made asserts (hopefully) side effect safe

--- testing/include/testing/asserts.h
+++ testing/include/testing/asserts.h
@@ -22,38 +22,61 @@
 
 #define ASSERT_BOOL(got, expected)                                             \
     do {                                                                       \
-        if ((got) != (expected)) {                                             \
-            const char* expected_str = expected ? "true" : "false";            \
-            const char* got_str = got ? "true" : "false";                      \
-            PRINT_ASSERT_ERR("Expected %s but got %s", expected_str, got_str); \
+        const bool assert_bool_got = (got);                                    \
+        const bool assert_bool_expected = (expected);                          \
+        if (assert_bool_got != assert_bool_expected) {                         \
+            const char* assert_bool_expected_str = assert_bool_expected        \
+                                                       ? "true"                \
+                                                       : "false";              \
+            const char* assert_bool_got_str = assert_bool_got ? "true"         \
+                                                              : "false";       \
+            PRINT_ASSERT_ERR("Expected %s but got %s",                         \
+                             assert_bool_expected_str,                         \
+                             assert_bool_got_str);                             \
         }                                                                      \
     } while (0)
 
 #define ASSERT_CHAR(got, expected)                                             \
     do {                                                                       \
-        if ((got) != (expected)) {                                             \
-            PRINT_ASSERT_ERR("Expected %c but got %c", expected, got);         \
+        const char assert_char_got = (got);                                    \
+        const char assert_char_expected = (expected);                          \
+        if (assert_char_got != assert_char_expected) {                         \
+            PRINT_ASSERT_ERR("Expected %c but got %c",                         \
+                             assert_char_expected,                             \
+                             assert_char_got);                                 \
         }                                                                      \
     } while (0)
 
 #define ASSERT_INT(got, expected)                                              \
     do {                                                                       \
-        if ((got) != (expected)) {                                             \
-            PRINT_ASSERT_ERR("Expected %d but got %d", expected, got);         \
+        const int assert_int_got = (got);                                      \
+        const int assert_int_expected = (expected);                            \
+        if (assert_int_got != assert_int_expected) {                           \
+            PRINT_ASSERT_ERR("Expected %d but got %d",                         \
+                             assert_int_expected,                              \
+                             assert_int_got);                                  \
         }                                                                      \
     } while (0)
 
 #define ASSERT_INTMAX_T(got, expected)                                         \
     do {                                                                       \
-        if ((got) != (expected)) {                                             \
-            PRINT_ASSERT_ERR("Expected %jd but got %jd", expected, got);       \
+        const intmax_t assert_intmax_t_got = (got);                            \
+        const intmax_t assert_intmax_t_expected = (expected);                  \
+        if (assert_intmax_t_got != assert_intmax_t_expected) {                 \
+            PRINT_ASSERT_ERR("Expected %jd but got %jd",                       \
+                             assert_intmax_t_expected,                         \
+                             assert_intmax_t_got);                             \
         }                                                                      \
     } while (0)
 
 #define ASSERT_UINTMAX_T(got, expected)                                        \
     do {                                                                       \
-        if ((got) != (expected)) {                                             \
-            PRINT_ASSERT_ERR("Expected %ju but got %ju", expected, got);       \
+        const intmax_t assert_uintmax_t_got = (got);                           \
+        const intmax_t assert_uintmax_t_expected = (expected);                 \
+        if (assert_uintmax_t_got != assert_uintmax_t_expected) {               \
+            PRINT_ASSERT_ERR("Expected %ju but got %ju",                       \
+                             assert_uintmax_t_expected,                        \
+                             assert_uintmax_t_got);                            \
         }                                                                      \
     } while (0)
 
@@ -70,36 +93,48 @@
 
 #define ASSERT_DOUBLE(got, expected, precision)                                \
     do {                                                                       \
-        if (fabsl((got) - (expected)) > precision) {                           \
-            PRINT_ASSERT_ERR("Expected %.20f but got %.20f", expected, got);   \
+        const double assert_double_got = (got);                                \
+        const double assert_double_expected = (expected);                      \
+        const double assert_double_precision = (precision);                    \
+        if (fabsl(assert_double_got - assert_double_expected)                  \
+            > assert_double_precision) {                                       \
+            PRINT_ASSERT_ERR("Expected %.20f but got %.20f",                   \
+                             assert_double_expected,                           \
+                             assert_double_got);                               \
         }                                                                      \
     } while (0)
 
 #define ASSERT_STR(got, expected)                                              \
     do {                                                                       \
-        if ((expected) == NULL && (got) != NULL) {                             \
-            PRINT_ASSERT_ERR("Expected NULL but got %s", got);                 \
-        } else if ((expected) != NULL && (got) == NULL) {                      \
-            PRINT_ASSERT_ERR("Expected %s but got NULL", expected);            \
-        } else if ((got) != NULL && (expected) != NULL) {                      \
-            if (strcmp(got, expected) != 0) {                                  \
-                PRINT_ASSERT_ERR("Expected %s but got %s", expected, got);     \
+        const char* const assert_str_got = (got);                              \
+        const char* const assert_str_expected = (expected);                    \
+        if (assert_str_expected == NULL && assert_str_got != NULL) {           \
+            PRINT_ASSERT_ERR("Expected NULL but got %s", assert_str_got);      \
+        } else if (assert_str_expected != NULL && assert_str_got == NULL) {    \
+            PRINT_ASSERT_ERR("Expected %s but got NULL", assert_str_expected); \
+        } else if (assert_str_got != NULL && assert_str_expected != NULL) {    \
+            if (strcmp(assert_str_got, assert_str_expected) != 0) {            \
+                PRINT_ASSERT_ERR("Expected %s but got %s",                     \
+                                 assert_str_expected,                          \
+                                 assert_str_got);                              \
             }                                                                  \
         }                                                                      \
     } while (0)
 
 #define ASSERT_NULL(got)                                                       \
     do {                                                                       \
-        if ((got) != NULL) {                                                   \
-            PRINT_ASSERT_ERR("Expected NULL, but got %p", (void*)got);         \
+        const void* const assert_null_got = (got);                             \
+        if (assert_null_got != NULL) {                                         \
+            PRINT_ASSERT_ERR("Expected NULL, but got %p", assert_null_got);    \
         }                                                                      \
     } while (0)
 
 #define ASSERT_NOT_NULL(got)                                                   \
     do {                                                                       \
-        if ((got) == NULL) {                                                   \
+        const void* const assert_not_null_got = (got);                         \
+        if (assert_not_null_got == NULL) {                                     \
             PRINT_ASSERT_ERR("Expected non null pointer, but got %p",          \
-                             (void*)got);                                      \
+                             assert_not_null_got);                             \
         }                                                                      \
     } while (0)
 
