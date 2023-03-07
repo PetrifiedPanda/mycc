2c64657b5718bf4e200021c8f2919cb1ac112f86
Benedikt <bfj.koehn@gmail.com>
2023-03-07 17:33:28 +0100

    Moved generic_assoc_list and generic_assoc into generic_sel.h/.c

--- frontend/include/frontend/ast/expr/generic_sel.h
+++ frontend/include/frontend/ast/expr/generic_sel.h
@@ -1,9 +1,27 @@
 #ifndef GENERIC_SEL_H
 #define GENERIC_SEL_H
 
+#include <stddef.h>
+#include <stdbool.h>
+
 #include "frontend/ast/ast_node_info.h"
 
-#include "generic_assoc_list.h"
+#include "frontend/parser/parser_state.h"
+
+struct type_name;
+struct assign_expr;
+
+struct generic_assoc {
+    struct ast_node_info info;
+    struct type_name* type_name; // if NULL this is the default case
+    struct assign_expr* assign;
+};
+
+struct generic_assoc_list {
+    struct ast_node_info info;
+    size_t len;
+    struct generic_assoc* assocs;
+};
 
 struct assign_expr;
 
@@ -16,6 +34,10 @@ struct generic_sel {
 struct generic_sel* parse_generic_sel(struct parser_state* s);
 
 void free_generic_sel(struct generic_sel* s);
+
+void free_generic_assoc_list(struct generic_assoc_list* l);
+
+void free_generic_assoc_children(struct generic_assoc* a);
 
 #include "assign_expr.h"
 
