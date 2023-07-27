fd54ae202fe137af4e327655bb858ca5a593ebe9
Koehn <bfj.koehn@gmail.com>
2023-07-27 15:49:37 +0200

    Continued preparation for new ast

--- frontend/include/frontend/ast/ast.h
+++ frontend/include/frontend/ast/ast.h
@@ -6,47 +6,47 @@ typedef enum {
     AST_TRANSLATION_UNIT,
     // lhs func_signature rhs compound_statement
     AST_FUNC_DEF,
-    // lhs declaration_specs, rhs is init_declarator_list 
+    // lhs declaration_specs rhs init_declarator_list 
     AST_DECLARATION,
-    // lhs is expr, rhs is String literal
+    // lhs expr rhs string_literal 
     AST_STATIC_ASSERT_DECLARATION,
-    // lhs is Declaration(?), rhs is DeclarationList
+    // lhs TODO:, rhs declaration_list 
     AST_FUNC_SIGNATURE,
     // subrange (declaration | statement)[lhs...rhs] 
     AST_COMPOUND_STATEMENT,
     // labeled_statement | compound_statement | expr_statement | selection_statement | iteration_statement | jump_statement (might be removeable)
     AST_STATEMENT,
-    // lhs identifier: rhs statement
+    // lhs identifier ':' rhs statement
     AST_LABELED_STATEMENT,
-    // case lhs const_expr: rhs statement
+    // 'case' lhs const_expr ':' rhs statement
     AST_CASE_STATEMENT,
-    // default: rhs (merge with case statement?)
+    // 'default' ':' rhs (merge with case statement?)
     AST_DEFAULT_STATEMENT,
-    // expr (might be removeable)
+    // expr ';' (might be removeable)
     AST_EXPR_STATEMENT,
-    // switch (lhs expr) rhs statement
+    // 'switch' '(' lhs expr ')' rhs statement
     AST_SWITCH_STATEMENT,
-    // if (lhs expr) rhs statement
+    // 'if' '(' lhs expr ')' rhs statement
     AST_SIMPLE_IF,
     // lhs simple_if else rhs statement
     AST_IF_ELSE,
-    // while (lhs expr) rhs statement
+    // 'while' '(' lhs expr ')' rhs statement
     AST_WHILE,
-    // lhs expr while (rhs expr);
+    // 'do' lhs statement while '(' rhs expr ')' ';'
     AST_DO_WHILE,
-    // for (lhs for_clause) rhs statement
+    // for '('lhs for_clause')' rhs statement
     AST_FOR,
     // lhs (expr_statement | declaration) rhs for_loop_actions 
     AST_FOR_CLAUSE,
     // lhs expr_statement rhs expr
     AST_FOR_LOOP_ACTIONS,
-    // goto lhs identifier;
+    // 'goto' lhs identifier ';'
     AST_GOTO_STATEMENT,
-    // continue;
+    // 'continue' ';'
     AST_CONTINUE_STATEMENT,
-    // break;
+    // 'break' ';'
     AST_BREAK_STATEMENT,
-    // return lhs expr;
+    // 'return' lhs expr ';'
     AST_RETURN_STATEMENT,
     // lhs (storage_class_spec | type_spec | func_spec | align_spec) rhs declaration_specifiers
     AST_DECLARATION_SPECS,
@@ -54,15 +54,17 @@ typedef enum {
     AST_STORAGE_CLASS_SPEC,
     // lhs ?(identifier | enum_spec | struct_union_spec | atomic_type_spec) (otherwise void char int float double bool atomic struct enum)
     AST_TYPE_SPEC,
-    // enum lhs ?identifier rhs enum_list 
+    // 'enum' lhs ?identifier rhs enum_list 
     AST_ENUM_SPEC,
     // subrange enumerator[lhs...rhs]
     AST_ENUM_LIST,
-    // lhs identifier = rhs ?const_expr
+    // lhs identifier '=' rhs ?const_expr
     AST_ENUMERATOR,
-    // (struct | union) lhs ?identifier rhs ?struct_declaration_list
+    // struct_union_spec:
     // needs either identifier or struct_declaration_list
+    // 'struct' lhs ?identifier rhs ?struct_declaration_list
     AST_STRUCT_SPEC,
+    // 'union' lhs ?identifier rhs ?struct_declaration_list
     AST_UNION_SPEC,
     // subrange (struct_declaration | static_assert_declaration)[lhs...rhs]
     AST_STRUCT_DECLARATION_LIST,
@@ -70,9 +72,9 @@ typedef enum {
     AST_STRUCT_DECLARATION,
     // subrange struct_declarator[lhs...rhs]
     AST_STRUCT_DECLARATOR_LIST,
-    // lhs declarator : rhs const_expr;
+    // lhs declarator ':' rhs const_expr;
     AST_STRUCT_DECLARATOR,
-    // _Atomic(lhs type_name)
+    // '_Atomic' '(' lhs type_name ')'
     AST_ATOMIC_TYPE_SPEC,
     // inline noreturn
     AST_FUNC_SPEC,
@@ -82,11 +84,11 @@ typedef enum {
     AST_ALIGN_SPEC_BYTES,
     // subrange init_declarator[lhs...rhs]
     AST_INIT_DECLARATOR_LIST,
-    // lhs declarator, rhs initializer
+    // lhs declarator rhs initializer
     AST_INIT_DECLARATOR,
-    // lhs pointer, rhs direct_declarator
+    // lhs pointer rhs direct_declarator
     AST_DECLARATOR,
-    // * lhs type_qualifier_list
+    // '*' lhs type_qualifier_list
     AST_POINTER,
     // lhs ?type_qual_list (const restrict volatile atomic)
     AST_TYPE_QUAL_LIST,
@@ -136,71 +138,114 @@ typedef enum {
     AST_DESIGNATOR,
     // cond_expr (might be removeable)
     AST_CONST_EXPR,
-    // lhs assign_expr, rhs ?assign_expr
+    // lhs assign_expr ',' rhs ?assign_expr
     AST_EXPR,
-    // lhs cond_expr assign_op rhs (assign_expr | cond_expr)
+    // assign_expr:
+    // lhs cond_expr '=' rhs (assign_expr | cond_expr)
     AST_ASSIGN,
+    // lhs cond_expr '*=' rhs (assign_expr | cond_expr)
     AST_ASSIGN_MUL,
+    // lhs cond_expr '/=' rhs (assign_expr | cond_expr)
     AST_ASSIGN_DIV,
+    // lhs cond_expr '%=' rhs (assign_expr | cond_expr)
     AST_ASSIGN_MOD,
+    // lhs cond_expr '+=' rhs (assign_expr | cond_expr)
     AST_ASSIGN_ADD,
+    // lhs cond_expr '-=' rhs (assign_expr | cond_expr)
     AST_ASSIGN_SUB,
+    // lhs cond_expr '<<=' rhs (assign_expr | cond_expr)
     AST_ASSIGN_SHL,
+    // lhs cond_expr '>>=' rhs (assign_expr | cond_expr)
     AST_ASSIGN_SHR,
+    // lhs cond_expr '&=' rhs (assign_expr | cond_expr)
     AST_ASSIGN_AND,
+    // lhs cond_expr '^=' rhs (assign_expr | cond_expr)
     AST_ASSIGN_XOR,
+    // lhs cond_expr '|=' rhs (assign_expr | cond_expr)
     AST_ASSIGN_OR,
-    // lhs log_or_expr ? rhs cond_items
+    // cond_expr:
+    // lhs log_or_expr '?' rhs cond_items
     AST_COND_EXPR,
-    // rhs (cond_expr | expr) : lhs (cond_expr | expr)
+    // rhs (cond_expr | expr) ':' lhs (cond_expr | expr)
     AST_COND_ITEMS,
-    // lhs log_and_expr || rhs (log_or_expr | log_and_expr)
+    // lhs log_and_expr '||' rhs (log_or_expr | log_and_expr)
     AST_LOG_OR_EXPR,
-    // lhs or_expr && rhs (log_and_expr | or_expr)
+    // lhs or_expr '&&' rhs (log_and_expr | or_expr)
     AST_LOG_AND_EXPR,
-    // lhs xor_expr | rhs (or_expr | xor_expr)
+    // lhs xor_expr '|' rhs (or_expr | xor_expr)
     AST_OR_EXPR,
-    // lhs and_expr ^ rhs (xor_expr | and_expr)
+    // lhs and_expr '^' rhs (xor_expr | and_expr)
     AST_XOR_EXPR,
-    // lhs eq_expr & rhs (and_expr | eq_expr)
+    // lhs eq_expr '&' rhs (and_expr | eq_expr)
     AST_AND_EXPR,
-    // lhs rel_expr eq_op rhs (eq_expr | rel_expr)
+    // eq_expr:
+    // lhs rel_expr '==' rhs (eq_expr | rel_expr)
     AST_EQ_EXPR,
+    // lhs rel_expr '!=' rhs (eq_expr | rel_expr)
     AST_NE_EXPR,
-    // lhs shift_expr rel_op rhs (rel_expr | shift_expr)
+    // rel_expr:
+    // lhs shift_expr '<' rhs (rel_expr | shift_expr)
     AST_REL_EXPR_LT,
+    // lhs shift_expr '>' rhs (rel_expr | shift_expr)
     AST_REL_EXPR_GT,
+    // lhs shift_expr '<=' rhs (rel_expr | shift_expr)
     AST_REL_EXPR_LE,
+    // lhs shift_expr '>=' rhs (rel_expr | shift_expr)
     AST_REL_EXPR_GE,
-    // lhs add_expr shift_op rhs (shift_expr | add_expr)
+    // shift_expr
+    // lhs add_expr '<<' rhs (shift_expr | add_expr)
     AST_LSHIFT_EXPR,
+    // lhs add_expr '>>' rhs (shift_expr | add_expr)
     AST_RSHIFT_EXPR,
-    // lhs mul_expr add_expr rhs (add_expr | mul_expr)
+    // add_expr:
+    // lhs mul_expr '+' rhs (add_expr | mul_expr)
     AST_ADD_EXPR,
+    // lhs mul_expr '-' rhs (add_expr | mul_expr)
     AST_SUB_EXPR,
-    // lhs cast_expr mul_op rhs (mul_expr | cast_expr)
+    // mul_expr:
+    // lhs cast_expr '*' rhs (mul_expr | cast_expr)
     AST_MUL_EXPR,
+    // lhs cast_expr '/' rhs (mul_expr | cast_expr)
     AST_DIV_EXPR,
+    // lhs cast_expr '%' rhs (mul_expr | cast_expr)
     AST_MOD_EXPR,
-    // (lhs type_name)rhs unary_expr
+    // '('lhs type_name')'rhs unary_expr
     AST_CAST_EXPR,
-    // lhs spec_qual_list, rhs abs_declartor
+    // lhs spec_qual_list rhs abs_declartor
     AST_TYPE_NAME,
-    // unary_op, rhs (postfix_expr | cast_expr | type_name) (TODO: lhs)
+    // unary_expr:
+    // ++ lhs (unary_expr | postfix_expr)
     AST_UNARY_EXPR_INC,
+    // -- lhs (unary_expr | postfix_expr)
     AST_UNARY_EXPR_DEC,
+    // 'sizeof' lhs (unary_expr | postfix_expr | type_name)
     AST_UNARY_EXPR_SIZEOF,
-    // lhs (postfix_expr | primary_expr | struct_literal)[rhs expr]
+    // '_Alignof' (lhs type_name)
+    AST_UNARY_EXPR_ALIGNOF,
+    // '&' lhs cast_expr
+    AST_UNARY_EXPR_ADDRESSOF,
+    // '*' lhs cast_expr
+    AST_UNARY_EXPR_DEREF,
+    // '+' lhs cast_expr
+    AST_UNARY_EXPR_PLUS,
+    // '-' lhs cast_expr
+    AST_UNARY_EXPR_MINUS,
+    // '~' lhs cast_expr
+    AST_UNARY_EXPR_BNOT,
+    // '!' lhs cast_expr
+    AST_UNARY_EXPR_NOT,
+    // postfix_expr:
+    // lhs (postfix_expr | primary_expr | compound_literal)[rhs expr]
     AST_POSTFIX_EXPR_INDEX,
     // lhs postfix_expr(rhs arg_expr_list)
     AST_POSTFIX_EXPR_BRACKET,
-    // lhs postfix_expr . rhs identifier
+    // lhs postfix_expr '.' rhs identifier
     AST_POSTFIX_ACCESS,
-    // lhs postfix_expr -> rhs identifier
+    // lhs postfix_expr '->' rhs identifier
     AST_POSTFIX_PTR_ACCESS,
-    // lhs postfix_expr ++
+    // lhs postfix_expr '++'
     AST_POSTFIX_INC,
-    // lhs postfix_expr --
+    // lhs postfix_expr '--'
     AST_POSTFIX_DEC,
     // subrange assign_expr[lhs...rhs]
     AST_ARG_EXPR_LIST,
@@ -212,8 +257,8 @@ typedef enum {
     // main token is string literal / __func__
     AST_STRING_LITERAL,
     AST_FUNC,
-    // TODO:
-    AST_STRUCT_LITERAL,
+    // '(' lhs type_name ')' '{' rhs init_list '}'
+    AST_COMPOUND_LITERAL,
     // main token is identifier
     AST_IDENTIFIER,
 } ASTNodeKind;
