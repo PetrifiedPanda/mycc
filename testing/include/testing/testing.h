5738c809ce96b35ba1953051f80cc2dcd150eb13
Benedikt Köhn <bfj.koehn@gmail.com>
2023-10-23 20:09:03 +0200

    Removed test main code

--- testing/include/testing/testing.h
+++ testing/include/testing/testing.h
@@ -81,83 +81,9 @@ extern jmp_buf test_jump_buf;
     }
 
 /**
- * Make a test suite defined in another .c file available
- */
-#define GET_EXTERN_SUITE(name) extern size_t name##_test_suite(void)
-
-/**
- * Forward declaration of test suite
- */
-#define TEST_SUITE_DECL(name) size_t name##_test_suite(void)
-
-/**
  * Begin a test, should be followed by new scope, as with a function
  */
 #define TEST(name) static void name##_test(void)
-
-#define TEST_SUITE_LIST_BEGIN(list_name)                                       \
-    size_t (*list_name##_test_suite_list[])(void) =
-
-#define TEST_SUITE_LIST_ITEM(suite_name) suite_name##_test_suite
-
-#define TEST_SUITE_LIST_END(list_name)                                         \
-    ;                                                                          \
-    enum {                                                                     \
-        list_name##_test_suite_list_size = sizeof(list_name##_test_suite_list) \
-                                           / sizeof(size_t(*)(void))           \
-    }
-
-/**
- * Begins the test main function, which returns EXIT_FAILURE if any test failed
- *
- * @param max_num_suites The maximum number of test suites in this main
- *                       must be a compile-time constant
- */
-#define TEST_MAIN_BEGIN(max_num_suites)                                        \
-    jmp_buf test_jump_buf;                                                     \
-    int main(void) {                                                           \
-        const struct timespec start = mycc_current_time();                     \
-        enum {                                                                 \
-            MAX_NUM_SUITES = (max_num_suites)                                  \
-        };                                                                     \
-        size_t (*test_suites[MAX_NUM_SUITES])(void);                           \
-        size_t num_suites = 0;
-
-#define TEST_MAIN_ADD(test_suite)                                              \
-    do {                                                                       \
-        assert(num_suites < MAX_NUM_SUITES);                                   \
-        test_suites[num_suites] = test_suite##_test_suite;                     \
-        ++num_suites;                                                          \
-    } while (0)
-
-#define TEST_MAIN_ADD_LIST(list_name)                                          \
-    do {                                                                       \
-        const size_t list_size = list_name##_test_suite_list_size;             \
-        assert(num_suites + list_size <= MAX_NUM_SUITES);                      \
-        for (size_t i = 0; i < list_size; ++i) {                               \
-            test_suites[num_suites] = list_name##_test_suite_list[i];          \
-            ++num_suites;                                                      \
-        }                                                                      \
-    } while (0)
-
-#define TEST_MAIN_END()                                                        \
-    size_t num_failed = 0;                                                     \
-    for (size_t i = 0; i < num_suites; ++i) {                                  \
-        num_failed += test_suites[i]();                                        \
-    }                                                                          \
-    const struct timespec end = mycc_current_time();                           \
-    const struct timespec diff = mycc_time_diff(&end, &start);                 \
-    const double msecs = mycc_get_msecs_double(&diff);                         \
-    if (num_failed == 0) {                                                     \
-        mycc_printf("All tests successful in {float} ms\n", msecs);            \
-        return EXIT_SUCCESS;                                                   \
-    } else {                                                                   \
-        mycc_printf("{size_t} tests failed in {float} ms\n",                   \
-                    num_failed,                                                \
-                    msecs);                                                    \
-        return EXIT_FAILURE;                                                   \
-    }                                                                          \
-    }
 
 /**
  * Prints an assert error, failing the test. Must not be called outside of tests
